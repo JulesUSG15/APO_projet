df25b66c7e43675ab9a67f3e6d78d43934efbd09
NicoutG <123987124+NicoutG@users.noreply.github.com>
2023-12-05 23:09:20 +0100

    Update Tableau.java

--- Tableau.java
+++ Tableau.java
@@ -1,11 +1,11 @@
 
 public class Tableau {
-    int dim;
-    int taille;
-    Object [] tab;
+    private int dim;
+    private int taille;
+    private Object [] tab;
     
     //d la dimension du tableau et t la taille
-    Tableau (int d, int t) {
+    public Tableau (int d, int t) {
         if (d>0 && t>0) {
             dim=d;
             taille=t;
@@ -26,14 +26,18 @@ public class Tableau {
             }
     }
     
+    public int getDim () {
+        return dim;
+    }
+    
+    public int getTaille () {
+        return taille;
+    }
+    
     //Les arguments sont des indices
-    int getVal (int... args) {
+    public int getVal (int... args) {
         if (args.length==dim) {
-            int arg0=args[0];
-            if (arg0<0 || arg0>=taille) {
-                System.out.println("get impossible : Les indices doivent etre inferieur à "+taille);
-                return -1;
-            }
+            int arg0=args[0]%taille;
             if (args.length==1) {
                 return (int)tab[arg0];
             }
@@ -48,25 +52,19 @@ public class Tableau {
     }
     
     //Les premiers arguments sont les indices et le dernier est la valeur
-    boolean setVal (int... args) {
+    public boolean setVal (int... args) {
         if (args.length==dim+1) {
-            int arg0=args[0];
-            if (0<=arg0 && arg0<taille) {
-                if (args.length==2) {
-                    tab[arg0]=args[1];
-                    return true;
-                }
-                else {
-                    int [] args2=new int [args.length-1];
-                    for (int i=0;i<args.length-1;i++) {
-                        args2[i]=args[i+1];
-                    }
-                    return ((Tableau)tab[arg0]).setVal(args2);
-                }
+            int arg0=args[0]%taille;
+            if (args.length==2) {
+                tab[arg0]=args[1];
+                return true;
             }
             else {
-                System.out.println("set impossible : Les indices doivent etre inferieur à "+taille);
-                return false;
+                int [] args2=new int [args.length-1];
+                for (int i=0;i<args.length-1;i++) {
+                    args2[i]=args[i+1];
+                }
+                return ((Tableau)tab[arg0]).setVal(args2);
             }
         }
         else {
