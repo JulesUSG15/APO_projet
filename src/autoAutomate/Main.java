d2310a5120e1b429bbc2e1600f4e43f6e4fa2675
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-12-19 15:19:58 +0100

    Amélioration du menu dans Main.java

--- src/autoAutomate/Main.java
+++ src/autoAutomate/Main.java
@@ -5,21 +5,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Veuillez choisir une option:");
-        System.out.println("1. JeuDeLaVie");
-        System.out.println("2. Automate1D");
+        System.out.println("1. Automate 1D");
+        System.out.println("2. Règle de majorité");
+        System.out.println("3. Jeu de la vie");
+        System.out.println("4. Feu de forêt");
+        System.out.println("0. Quitter");
+        System.out.print("Choisissez une option : ");
 
         int option = scanner.nextInt();
 
         switch (option) {
             case 1:
-                JeuDeLaVie.main(args);
-                break;
-            case 2:
                 Automate1D.main(args);
                 break;
+            case 2:
+                //RegleMajorite.main(args);
+                break;
+            case 3:
+                JeuDeLaVie.main(args);
+                break;
+            case 4:
+                //FeuDeForet.main(args);
+                break;
+            case 0:
+                System.out.println("Au revoir !");
+                break;
             default:
-                System.out.println("Option non reconnue");
+                System.out.println("Option invalide. Veuillez réessayer.");
                 break;
         }
 
