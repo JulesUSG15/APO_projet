b8e496b444398aad993a4850e5b12e9ff9ef7b5b
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-12-19 16:45:53 +0100

    Soit remplissage à la main du tableau ou random pour le jeu de la vie

--- src/autoAutomate/JeuDeLaVie.java
+++ src/autoAutomate/JeuDeLaVie.java
@@ -1,24 +1,54 @@
 package src.autoAutomate;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class JeuDeLaVie {
     
     public static void main(String[] args) {
         Regles reg=new Regles();
         System.out.println(reg.charger("data/jeu_vie.dac"));
         System.out.println(reg.getExp());
-        Tableau tab=new Tableau (2,11);
-        tab.setVal(4,4,1);
-        tab.setVal(4,5,1);
-        tab.setVal(4,6,1);
-        tab.setVal(5,3,1);
-        tab.setVal(5,7,1);
-        tab.setVal(6,4,1);
-        tab.setVal(6,5,1);
-        tab.setVal(6,6,1);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Veuillez entrer la taille du tableau : ");
+        int taille = scanner.nextInt();
+
+        Tableau tab=new Tableau (2, taille);
+
+        System.out.println("Voulez-vous remplir le tableau manuellement ou de manière aléatoire ?");
+        System.out.println("1. Manuellement");
+        System.out.println("2. Aléatoirement");
+        int choix = scanner.nextInt();
+
+        if (choix == 1) {
+            for (int j = 0; j < taille; j++) {
+                for (int i = 0; i < taille; i++) {
+                    System.out.print("Veuillez entrer la valeur pour la position (" + j + ", " + i + ") : ");
+                    int valeur = scanner.nextInt();
+                    tab.setVal(j, i, valeur);
+                }
+            }
+        } else {
+            initialiserTableauAleatoire(tab);
+        }
+
         afficher(tab);
         for (int i=0;i<8;i++) {
             tab=reg.appliquer(tab);
             afficher(tab);
+        }
+
+        scanner.close();
+    }
+
+    public static void initialiserTableauAleatoire(Tableau tab) {
+        Random random = new Random();
+        for (int j = 0; j < tab.getTaille(); j++) {
+            for (int i = 0; i < tab.getTaille(); i++) {
+                tab.setVal(j, i, random.nextInt(2));
+            }
         }
     }
 
