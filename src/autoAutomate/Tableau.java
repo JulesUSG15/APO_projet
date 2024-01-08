329ea225116a16e6287afbc83d067e39c854b3e4
NicoutG <nicogallet01@gmail.com>
2024-01-08 15:36:45 +0100

    debut chargement sauvegarde

--- src/autoAutomate/Tableau.java
+++ src/autoAutomate/Tableau.java
@@ -1,5 +1,9 @@
 package src.autoAutomate;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Tableau {
     private int dim;
     private int taille;
@@ -10,14 +14,14 @@ public class Tableau {
         if (d>0 && t>0) {
             dim=d;
             taille=t;
-            tab=new Object [t];
-            if (d>1) {
-                for (int i=0;i<t;i++) {
-                    tab[i]=new Tableau(d-1,t);
+            tab=new Object [taille];
+            if (dim>1) {
+                for (int i=0;i<taille;i++) {
+                    tab[i]=new Tableau(dim-1,taille);
                 }
             }
             else {
-                for (int i=0;i<t;i++) {
+                for (int i=0;i<taille;i++) {
                     tab[i]=0.0;
                 }
             }
@@ -25,6 +29,118 @@ public class Tableau {
         else {
             System.out.println("creation impossible : La dimension et la taille doivent etre superieur à 0");
             }
+    }
+
+    public Tableau (String fichier) {
+        if (!charger(fichier)) {
+            System.out.println("creation impossible : Impossible de lire le fichier");
+        }
+    }
+
+    private String [] simplification (String exp) {
+        String [] exps=exp.split("\r?\n|\r");
+        exp="";
+        for (int i=0;i<exps.length;i++) {
+            exp+=exps[i]+" ";
+        }
+        exps=exp.split(" ");
+        int t=0;
+        for (int i=0;i<exps.length;i++) {
+            if (!exps[i].equals("")) {
+                t++;
+            }
+        }
+        if (t!=exps.length) {
+            String [] exps2=new String [t];
+            int indice=0;
+            for (int i=0;i<exps.length;i++) {
+                if (!exps[i].equals("")) {
+                    exps2[indice]=exps[i];
+                    indice++;
+                }
+            }
+            exps=exps2;
+        }
+        return exps;
+    }
+
+    public boolean charger (String fichier) {
+        try {
+            String exp=new String(Files.readAllBytes(Paths.get(fichier)));
+            String [] exps=simplification(exp);
+            if (exps==null || exps.length<2) {
+                return false;
+            }
+            System.out.println(" ");
+            for (int i=0;i<exps.length;i++) {
+                System.out.println(exps[i]);
+            }
+            System.out.println(" ");
+            dim=Integer.parseInt(exps[1]);
+            taille=Integer.parseInt(exps[3]);
+            tab=new Object [taille];
+            if (dim>1) {
+                for (int i=0;i<taille;i++) {
+                    tab[i]=new Tableau(dim-1,taille);
+                }
+            }
+            else {
+                for (int i=0;i<taille;i++) {
+                    tab[i]=0.0;
+                }
+            }
+            if (exps.length!=4+Math.pow(taille,dim)) {
+                return false;
+            }
+            int [] indices= new int [dim];
+            int num=4;
+            while (indices[0]<taille) {
+                for (int i=0;i<dim;i++) {
+                    if (indices[i]>=taille) {
+                        indices[i]=0;
+                    }
+                }
+                setVal(indices,Double.valueOf(exps[num]));
+                num++;
+                indices[dim-1]++;
+                for (int i=dim-2;i>=0;i--) {
+                    if (indices[i+1]>=taille) {
+                        indices[i]++;
+                    }
+                }
+            }
+            return true;
+        }
+        catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean sauvegarder (String fichier) {
+        try {
+            String exp=new String ();
+            exp="Dim: "+dim+" Taille: "+taille+"?\r\n";
+            int [] indices= new int [dim];
+            while (indices[0]<taille) {
+                for (int i=0;i<dim;i++) {
+                    if (indices[i]>=taille) {
+                        indices[i]=0;
+                    }
+                }
+                
+                
+                indices[dim-1]++;
+                for (int i=dim-2;i>=0;i--) {
+                    if (indices[i+1]>=taille) {
+                        indices[i]++;
+                    }
+                }
+            }
+            Files.write(Paths.get(fichier), exp.getBytes());
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
     
     public int getDim () {
