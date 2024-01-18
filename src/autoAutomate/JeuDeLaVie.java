6dd2d205cac40e8287c2cbd4eb4ed84c33187414
NicoutG <nicogallet01@gmail.com>
2024-01-18 16:03:49 +0100

    Jeu de la vie

--- src/autoAutomate/JeuDeLaVie.java
+++ src/autoAutomate/JeuDeLaVie.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
+import java.awt.Color;
 
 public class JeuDeLaVie extends JFrame implements ActionListener  {
 
@@ -13,11 +14,14 @@ public class JeuDeLaVie extends JFrame implements ActionListener  {
 
     // Interface graphique
     private int width = 500;
-    private int frameDisplayed = 0;
+    private int frameDisplayed = 0, etapes;
     private Turtle turtle = new Turtle();
     private JComboBox<String> choixGeneration;
-    private JTextField fieldEtapes, fieldTaille;
-    private JButton btnSimulation;
+    private JTextField fieldEtapes, fieldTaille, fieldDensite, fieldCharger, fieldSauvegarder;
+    private JButton btnSimulation, btnPrepare, btnSetTaille, btnSetDensite, btnSetCharger, btnSetSauvegarder;
+    private JButton [][] tableau;
+    private Tableau tab;
+    private JFrame f;
 
     public JeuDeLaVie() {
         this.reg = new Regles();
@@ -47,7 +51,9 @@ public class JeuDeLaVie extends JFrame implements ActionListener  {
 
         // Création de l'interface graphique
 
-        JFrame f = new JFrame("Simulation - Jeu de la vie");
+        tab=new Tableau(2,10);
+
+        f = new JFrame("Simulation - Jeu de la vie");
         
         f.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -58,31 +64,19 @@ public class JeuDeLaVie extends JFrame implements ActionListener  {
         JLabel titre = new JLabel("Jeu de la vie");
         titre.setBounds(20,10,200,30);
 
-        JLabel labelTaille = new JLabel("Taille du tableau :");
-        labelTaille.setBounds(20,40,120,30);
-        fieldTaille = new JTextField("10");
-        fieldTaille.setBounds(20,70,120,30);
-
         JLabel labelEtapes = new JLabel("Nombre d'étapes :");
-        labelEtapes.setBounds(150,40,120,30);
+        labelEtapes.setBounds(20,40,120,30);
         fieldEtapes = new JTextField("5");
-        fieldEtapes.setBounds(150,70,120,30);
+        fieldEtapes.setBounds(20,70,120,30);
 
-        String[] choices = { "Initailisation aléatoire","Initailisation manuelle"};
-        choixGeneration = new JComboBox<String>(choices);
-        choixGeneration.setBounds(280,70,180,30);
-
-        btnSimulation = new JButton("Lancer la simulation");
-        btnSimulation.setBounds(20,120,150,30);
-        btnSimulation.addActionListener(this);
+        btnPrepare = new JButton("Préparer la simulation");
+        btnPrepare.setBounds(20,150,170,30);
+        btnPrepare.addActionListener(this);
 
         f.add(titre);
-        f.add(labelTaille);
-        f.add(fieldTaille);
         f.add(labelEtapes);
         f.add(fieldEtapes);
-        f.add(choixGeneration);
-        f.add(btnSimulation);
+        f.add(btnPrepare);
         f.setSize(500,500);
         f.setLayout(null);
         f.setVisible(true);
@@ -91,32 +85,90 @@ public class JeuDeLaVie extends JFrame implements ActionListener  {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        // Lancement de la simulation
-        if(e.getSource() == btnSimulation){
-            int taille = 0;
-            int etapes = 0;
+        // Lancement de la préparation
+        if (e.getSource() == btnPrepare) {
             try {
-                taille = Integer.parseInt(fieldTaille.getText());
                 etapes = Integer.parseInt(fieldEtapes.getText());
             } catch (NumberFormatException ex) {
                 System.out.println("Veuillez entrer des valeurs correctes");
                 return;
             }
-
-            // On verifie que les champs ne sont pas vides et que les valeurs sont correctes
-            if (taille < 1 || etapes < 0) {
+            if (etapes < 0) {
                 System.out.println("Veuillez entrer des valeurs correctes");
                 return;
             }
+            pagePreparation();
+        }
 
-            Tableau tab = new Tableau (2, taille);
+        // Modif de la taille du tableau
+        if (e.getSource() == btnSetTaille) {
+            int taille=0;
+            try {
+                taille=Integer.parseInt(fieldTaille.getText());
+            } catch (NumberFormatException ex) {
+                System.out.println("Veuillez entrer des valeurs correctes");
+                return;
+            }
+            if (taille < 1) {
+                System.out.println("Veuillez entrer des valeurs correctes");
+                return;
+            }
+            tab=new Tableau (2,taille);
+            f.dispose();
+            pagePreparation();
+        }
+        // Generation aleatoire du tableau
+        if (e.getSource()==btnSetDensite) {
+            double densite=0;
+            try {
+                densite=Double.parseDouble(fieldDensite.getText());
+            } catch (NumberFormatException ex) {
+                System.out.println("Veuillez entrer des valeurs correctes");
+                return;
+            }
+            if (densite < 0 || 1<densite) {
+                System.out.println("Veuillez entrer des valeurs correctes");
+                return;
+            }
+            initialiserTableauAleatoire(densite);
+            f.dispose();
+            pagePreparation();
+        }
+        // Chargement du fichier
+        if (e.getSource()==btnSetCharger) {
+            Tableau nouv=new Tableau (fieldCharger.getText());
+            if (nouv==null || nouv.getDim()!=2) {
+                System.out.println("Veuillez entrer des valeurs correctes");
+                return;
+            }
+            tab=nouv;
+            f.dispose();
+            pagePreparation();
+        }
+        // Sauvegarde du fichier
+        if (e.getSource()==btnSetSauvegarder) {
+            tab.sauvegarder(fieldSauvegarder.getText());
+            System.out.println("Tableau sauvegardé");
+        }
+        // Modif du tableau
+        if (tab!=null) {
+            for (int i=0;i<tab.getTaille();i++) {
+                for (int j=0;j<tab.getTaille();j++) {
+                    if (e.getSource() == tableau[i][j]) {
+                        tab.setVal(i,j,(tab.getVal(i,j)+1)%2);
+                        switch ((int)tab.getVal(i,j)) {
+                            case 1: tableau[i][j].setBackground(Color.BLACK);break;
+                            default: {
+                                tableau[i][j].setBackground(Color.WHITE);
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-            if (choixGeneration.getSelectedItem() == "Initailisation manuelle") {
-                initialiserTableauManuelle(tab);
-            }
-            else {
-                initialiserTableauAleatoire(tab);
-            }
+        // Lancement de la simulation
+        if(e.getSource() == btnSimulation){
 
             //  On lance la simulation
             simuler(tab, etapes);
@@ -138,13 +190,85 @@ public class JeuDeLaVie extends JFrame implements ActionListener  {
         }
     }
 
-    public void initialiserTableauAleatoire(Tableau tab) {
-        Random random = new Random();
-        tab.remplir(random.nextInt((int)Math.pow(tab.getTaille(),tab.getDim())),1);
+    public void pagePreparation () {
+        f = new JFrame("Préparation - Feu de forêt");
+        
+        JLabel labelTaille = new JLabel("Taille du tableau :");
+        labelTaille.setBounds(20,20,120,30);
+        fieldTaille = new JTextField(""+tab.getTaille());
+        fieldTaille.setBounds(150,20,120,30);
+        btnSetTaille = new JButton("Changer la taille");
+        btnSetTaille.setBounds(280,20,170,30);
+        btnSetTaille.addActionListener(this);
+
+        JLabel labelDensite = new JLabel("Densité :");
+        labelDensite.setBounds(20,50,130,30);
+        fieldDensite = new JTextField("0.5");
+        fieldDensite.setBounds(150,50,120,30);
+        btnSetDensite = new JButton("Générer aléatoirement");
+        btnSetDensite.setBounds(280,50,170,30);
+        btnSetDensite.addActionListener(this);
+
+        JLabel labelCharger = new JLabel("Tableau à charger :");
+        labelCharger.setBounds(20,80,120,30);
+        fieldCharger = new JTextField("data/tableaux/nom.txt");
+        fieldCharger.setBounds(150,80,120,30);
+        btnSetCharger = new JButton("Charger le tableau");
+        btnSetCharger.setBounds(280,80,170,30);
+        btnSetCharger.addActionListener(this);
+
+        JLabel labelSauvegarder = new JLabel("Sauvegarde du tableau :");
+        labelSauvegarder.setBounds(20,110,120,30);
+        fieldSauvegarder = new JTextField("data/tableaux/nom.txt");
+        fieldSauvegarder.setBounds(150,110,120,30);
+        btnSetSauvegarder = new JButton("Sauvegarder le tableau");
+        btnSetSauvegarder.setBounds(280,110,170,30);
+        btnSetSauvegarder.addActionListener(this);
+
+        btnSimulation = new JButton("Lancer la simulation");
+        btnSimulation.setBounds(150,150,170,30);
+        btnSimulation.addActionListener(this);
+
+        tableau = new JButton [tab.getTaille()][tab.getTaille()];
+        majTableau();
+
+        f.add(labelTaille);
+        f.add(fieldTaille);
+        f.add(btnSetTaille);
+        f.add(labelDensite);
+        f.add(fieldDensite);
+        f.add(btnSetDensite);
+        f.add(labelCharger);
+        f.add(fieldCharger);
+        f.add(btnSetCharger);
+        f.add(labelSauvegarder);
+        f.add(fieldSauvegarder);
+        f.add(btnSetSauvegarder);
+        f.add(btnSimulation);
+        f.setSize(width,200+width);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 
-    public void initialiserTableauManuelle(Tableau tab) {
-        System.out.println("Initialisation manuelle");
+    public void majTableau () {
+        double step = 0.92*width / tab.getTaille();
+        for (int i=0;i<tab.getTaille();i++) {
+            for (int j=0;j<tab.getTaille();j++) {
+                tableau[i][j]=new JButton ();
+                tableau[i][j].setBounds((int)(12+i*step),(int)(200+j*step),(int)(step),(int)(step));
+                switch ((int)tab.getVal(i,j)) {
+                    case 1: tableau[i][j].setBackground(Color.BLACK);break;
+                    default: tableau[i][j].setBackground(Color.WHITE);
+                }
+                tableau[i][j].addActionListener(this);
+                f.add(tableau[i][j]);
+            }
+        }
+    }
+
+    public void initialiserTableauAleatoire(double densite) {
+        tab=new Tableau(2,tab.getTaille());
+        tab.remplir((int)(Math.pow(tab.getTaille(),tab.getDim())*densite),1);
     }
 
     public void simuler(Tableau tab, int n) {
